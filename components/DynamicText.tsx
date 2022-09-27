8a3ea43d4b4bdfb3d29c26487f00ac50b82b9832
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-09-27 16:16:35 +0900

    feat: text wrap

--- components/DynamicText.tsx
+++ components/DynamicText.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Container, Text } from "@chakra-ui/react";
 import { forwardRef, SetStateAction, useImperativeHandle, useState } from "react";
 
 const DynamicText = forwardRef((_props, ref) => {
@@ -10,7 +10,15 @@ const DynamicText = forwardRef((_props, ref) => {
     },
   }));
 
-  return <Text fontSize="5xl">{value}</Text>;
+  return (
+    <Container maxW="md">
+      <Box mb={5} shadow="md" borderWidth="1px">
+        <Text p={2} minH={10}>
+          {value}
+        </Text>
+      </Box>
+    </Container>
+  );
 });
 
 export default DynamicText;
