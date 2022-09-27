116fbcce132f9212a41786c16dcd06cf579122e8
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-09-27 18:07:18 +0900

    feat: add style for login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 };
---
+++ pages/auth.tsx
@@ -0,0 +1,10 @@
+import Signin from "components/Signin";
+import React from "react";
+
+export default function auth() {
+  return (
+    <div>
+      <Signin />
+    </div>
+  );
+}
