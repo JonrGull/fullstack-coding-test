00c628afc27eedb67f85b4bb29f1131efc59a722
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-02 12:03:34 +0900

    feat: form error handling

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,25 +11,31 @@ import {
   Stack,
   Text,
   useColorModeValue,
-} from "@chakra-ui/react";
-import { useAuth } from "context/AuthContext";
-import { useRouter } from "next/router";
-import { useRef } from "react";
+} from '@chakra-ui/react';
+import ErrorMessage from 'components/ErrorMessage';
+import { useAuth } from 'context/AuthContext';
+import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
 
 export default function Signin() {
   const { login } = useAuth();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const router = useRouter();
 
   const handleLogin = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-
     try {
+      setIsLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
       router.push("/");
+      setIsLoading(false);
     } catch (error) {
+      setError("Invalid username or password");
       console.log(error);
+      setIsLoading(false);
     }
   };
 
@@ -41,29 +47,26 @@ export default function Signin() {
         </Stack>
         <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8}>
           <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" ref={emailRef} />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" ref={passwordRef} />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack direction={{ base: "column", sm: "row" }} align={"start"} justify={"space-between"}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={"blue.400"}>Forgot password?</Link>
+            <form onSubmit={handleLogin}>
+              {error && <ErrorMessage message={error} />}
+              <FormControl isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" ref={emailRef} />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" ref={passwordRef} minLength={6} />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack direction={{ base: "column", sm: "row" }} align={"start"} justify={"space-between"}>
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={"blue.400"}>Forgot password?</Link>
+                </Stack>
+                <Button type="submit" isLoading={isLoading} colorScheme="blue">
+                  Sign in
+                </Button>
               </Stack>
-              <Button
-                onClick={handleLogin}
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}>
-                Sign in
-              </Button>
-            </Stack>
+            </form>
           </Stack>
 
           <Stack pt={6}>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+
 import { useAuth } from "context/AuthContext";
 import { useRouter } from "next/router";
 import { useRef, useState } from "react";
@@ -22,6 +23,9 @@ export default function Signup() {
   const { signUp } = useAuth();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const checkPasswordRef = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const router = useRouter();
 
   const [showPassword, setShowPassword] = useState(false);
@@ -30,9 +34,12 @@ export default function Signup() {
     e.preventDefault();
 
     try {
+      setIsLoading(true);
       await signUp(emailRef.current.value, passwordRef.current.value);
       router.push("/");
+      setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       console.log(error);
     }
   };
@@ -47,34 +54,28 @@ export default function Signup() {
         </Stack>
         <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8}>
           <Stack spacing={4}>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" ref={emailRef} />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? "text" : "password"} ref={passwordRef} />
-                <InputRightElement h={"full"}>
-                  <Button variant={"ghost"} onClick={() => setShowPassword((showPassword) => !showPassword)}>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                onClick={handleSignUp}
-                loadingText="Submitting"
-                size="lg"
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}>
-                Sign up
-              </Button>
-            </Stack>
+            <form onSubmit={handleSignUp}>
+              <FormControl isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" ref={emailRef} />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input type={showPassword ? "text" : "password"} ref={passwordRef} minLength={6} />
+                  <InputRightElement h={"full"}>
+                    <Button variant={"ghost"} onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button type="submit" loadingText="Submitting" isLoading={isLoading} colorScheme="blue">
+                  Sign up
+                </Button>
+              </Stack>
+            </form>
             <Stack pt={6}>
               <Text align={"center"}>
                 Already a user?{" "}
