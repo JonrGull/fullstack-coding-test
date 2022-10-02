ae71235731ecc3d0cf4cae70598b9c7caf7e09bb
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-02 13:20:04 +0900

    feat: add confetti

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Input } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRef } from "react";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 import DynamicText from "components/DynamicText";
 import styles from "styles/Home.module.css";
@@ -11,6 +13,8 @@ const Home = () => {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dynamicTextRef.current.changeValue(e.target.value);
   };
+
+  const { width, height } = useWindowSize();
 
   return (
     <div className={styles.container}>
@@ -23,6 +27,7 @@ const Home = () => {
         <DynamicText ref={dynamicTextRef} />
         <Input onChange={onChange} />
       </main>
+      <Confetti width={width} height={height} />
     </div>
   );
 };
