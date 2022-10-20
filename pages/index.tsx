2d77ef801aed521b90f82b2e8a7d1f748b99efd1
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-20 13:56:23 +0900

    chore: change package and confetti method

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,26 @@
 import { Box, Flex, Input, Stack, useColorModeValue } from "@chakra-ui/react";
 import DynamicText from "components/DynamicText";
 import { Private } from "config/firebase/authRoute";
+import JSConfetti from "js-confetti";
 import { useRef } from "react";
-import Confetti from "react-confetti";
-import useWindowSize from "react-use/lib/useWindowSize";
 
 const Home = () => {
   const dynamicTextRef = useRef(null);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dynamicTextRef.current.changeValue(e.target.value);
-  };
+    const jsConfetti = new JSConfetti();
 
-  const { width, height } = useWindowSize();
+    jsConfetti.addConfetti({
+      emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🌸"],
+      confettiRadius: 6,
+      confettiNumber: 3,
+    });
+  };
 
   return (
     <Flex minH={"93vh"} align={"center"} justify={"center"} bg={useColorModeValue("gray.50", "gray.800")}>
       <title>Coding Test</title>
-      <Confetti width={width} height={height} />
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}></Stack>
         <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8}>
