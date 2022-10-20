63748e8214b404c3b3bd61e41d4600a0afc86dbc
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-10-20 16:46:38 +0900

    feat: enchanced secret

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -1,12 +1,29 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Center, Text } from "@chakra-ui/react";
 import { Private } from "config/firebase/authRoute";
+import { useAuth } from "context/AuthContext";
+import { useState } from "react";
+import styles from "styles/secret.module.css";
 
-const secret = () => {
+const Secret = () => {
+  const { user } = useAuth();
+  const [reveal, setReveal] = useState(false);
+
+  const handleShowText = () => {
+    setReveal((curr) => !curr);
+  };
+
   return (
-    <Text fontSize="sm" color="white">
-      You found me!
-    </Text>
+    <>
+      <Button onClick={handleShowText} size="xs" rounded="5px" colorScheme="blackAlpha" variant="ghost">
+        SECRET
+      </Button>
+      <Center mt={100}>
+        <Text ml={1} fontSize="5xl" color={"white"}>
+          {reveal && <span className={styles.secret}>{user.email} is really awesome.</span>}
+        </Text>
+      </Center>
+    </>
   );
 };
 
-export default Private(secret);
+export default Private(Secret);
